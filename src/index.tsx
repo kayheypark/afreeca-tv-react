2ec79f9d144855e30b33a303d73a68e9e550072e
Gyeongho Park <59360980+kayheypark@users.noreply.github.com>
2022-12-09 00:32:16 +0900

    router : setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from 'page/Home';
+import Mypage from 'page/Mypage';
 
-ReactDOM.render(
+const router = createBrowserRouter([
+    { path: '/', element: <Home /> },
+    { path: '/mypage', element: <Mypage /> },
+]);
+
+createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>,
-    document.getElementById('root'),
 );
