4115f0cba743d2aa09700d1a6142fdba90d0e2ec
Gyeongho Park <59360980+kayheypark@users.noreply.github.com>
2022-12-09 01:51:03 +0900

    markup : BaseLayout

--- src/layout/BaseLayout.tsx
+++ src/layout/BaseLayout.tsx
@@ -24,7 +24,24 @@ const BaseLayout = ({ children }: IProps) => (
             </div>
         </Header>
         <View>
-            <div>사이드바</div>
+            <div>
+                <ul>
+                    <li>홈</li>
+                    <li>2022 카타르월드컵</li>
+                    <li>2022 BJ대상</li>
+                    <li>e스포츠</li>
+                    <li>VOD</li>
+                    <li>MY</li>
+                </ul>
+                <hr />
+                <ul>
+                    <li>샵프리카</li>
+                    <li>랭킹</li>
+                    <li>소통센터</li>
+                    <li>고객센터</li>
+                    <li>이벤트</li>
+                </ul>
+            </div>
             <main>{children}</main>
         </View>
     </>
