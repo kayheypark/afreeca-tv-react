eaf5aef3d9a70b76b8911a1f54dbd6be1a62fdeb
Gyeongho Park <59360980+kayheypark@users.noreply.github.com>
2022-12-09 01:54:14 +0900

    code clean: GlobalStyle

--- src/style/GlobalStyle.tsx
+++ src/style/GlobalStyle.tsx
@@ -1,32 +1,32 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-*, *::before, *::after {
-    box-sizing: border-box;
-}
+    *, *::before, *::after {
+        box-sizing: border-box;
+    };
 
-body {
-    font-size: 14px;
-    line-height: 1;
-    font-family: Arial, Helvetica, sans-serif;
-    margin: 0;
-    padding: 0;
-}
+    body {
+        font-size: 14px;
+        line-height: 1;
+        font-family: Arial, Helvetica, sans-serif;
+        margin: 0;
+        padding: 0;
+    }
 
-h1,h2,h3,h4,h5,h6 {
-    margin: 0;
-    padding: 0;
-}
+    h1,h2,h3,h4,h5,h6 {
+        margin: 0;
+        padding: 0;
+    }
 
-ul, ol, dl, li, dd {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-}
+    ul, ol, dl, li, dd {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+    }
 
-button {
-    cursor: pointer;
-}
+    button {
+        cursor: pointer;
+    }
 `;
 
 export default GlobalStyle;
