b5bad8f018369c04f8d254232e9ae172b03d9394
Gyeongho Park <59360980+kayheypark@users.noreply.github.com>
2022-12-12 03:09:34 +0900

    feat: handleClickOutside

--- src/component/template/GlobalSearch.tsx
+++ src/component/template/GlobalSearch.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import FlexX from 'component/parts/FlexX';
 import FlexY from 'component/parts/FlexY';
 
 const GlobalSearch = () => {
+    const wrapperRef = useRef<HTMLDivElement>(null);
+
     const [isActive, setIsActive] = useState<boolean>(false);
     const [keyword, setKeyword] = useState<string>('');
 
-    const handleClcikRemoveKeyword = () => {
+    const handleClickRemoveKeyword = () => {
         setKeyword('');
     };
 
+    const handleClickOutside = (e: any) => {
+        if (!wrapperRef.current?.contains(e.target)) {
+            setIsActive(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('click', handleClickOutside);
+        return () => {
+            window.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
+
     return (
-        <Wrapper>
+        <Wrapper ref={wrapperRef} isActive={isActive} onClick={() => setIsActive(true)}>
             <InputWrapper>
-                <input
-                    type="text"
-                    value={keyword}
-                    onChange={(e) => {
-                        setKeyword(e.target.value);
-                    }}
-                />
-                <button type="button" onClick={handleClcikRemoveKeyword}>
+                <input type="text" value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+                <button type="button" onClick={handleClickRemoveKeyword}>
                     검색어 지우기
                 </button>
-                <button
-                    type="button"
-                    onClick={() => {
-                        console.info(keyword);
-                    }}
-                >
+                <button type="button" onClick={() => console.info(keyword)}>
                     검색
                 </button>
             </InputWrapper>
@@ -40,7 +44,12 @@ const GlobalSearch = () => {
 
 export default GlobalSearch;
 
-const Wrapper = styled(FlexY).attrs({ as: 'div' })``;
+const Wrapper = styled(FlexY).attrs({ as: 'div' })<{ isActive: boolean }>`
+    padding: 12px;
+    border: ${(props) => (props.isActive ? '1px solid #ecf0f5' : 'none')};
+    box-shadow: ${(props) => (props.isActive ? '0 0 4px #efefef' : 'none')};
+`;
+
 const InputWrapper = styled(FlexX).attrs({ as: 'div' })`
     border: 1px solid #4279ff;
     border-radius: 36px;
