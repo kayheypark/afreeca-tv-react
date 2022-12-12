8146b51340aad488d201b6e0171a8103337444ce
Gyeongho Park <59360980+kayheypark@users.noreply.github.com>
2022-12-13 01:46:28 +0900

    update: Colors

--- src/style/variable/color.ts
+++ src/style/variable/color.ts
@@ -1,9 +1,14 @@
 export const Color = {
     Blue: '#4279ff',
+    BlueOC: 'f2f8ff',
     Gray: '#e5ebf2',
 };
 
-export const ColorMean = {};
+export const ColorMean = {
+    Primary: Color.Blue,
+};
 export const ColorUI = {
     LayoutDivider: Color.Gray,
+    GNBActiveBG: Color.BlueOC,
+    GNBActiveColor: ColorMean.Primary,
 };
